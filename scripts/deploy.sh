c2f867147e8bf13ed0a9200c565e2ed76b51bc39
v.kugay <v.kugay@tinkoff.ru>
2021-10-10 16:14:50 +0300

    codeclimate: add users unit test

---
+++ scripts/deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+coverage() {
+  curl -L https://codeclimate.com/downloads/test-reporter/test-reporter-latest-linux-amd64 > ./cc-test-reporter
+  chmod +x ./cc-test-reporter
+  ./cc-test-reporter before-build
+}
+
+database() {
+  psql -U postgres -a -f ./database/bootstrap.sql
+}
+
+envs() {
+  cp .env.example .env  
+}
