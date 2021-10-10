51d9e20e27a26e360839e50ed5b1af4adbb72bc9
v.kugay <v.kugay@tinkoff.ru>
2021-10-10 16:30:39 +0300

    codeclimate: add users unit test

--- scripts/before_script.sh
+++ scripts/before_script.sh
@@ -2,20 +2,20 @@
 
 pwd
 
-function coverage() {
-  curl -L https://codeclimate.com/downloads/test-reporter/test-reporter-latest-linux-amd64 > ./cc-test-reporter
-  chmod +x ./cc-test-reporter
-  ./cc-test-reporter before-build
-}
+# function coverage() {
+#   curl -L https://codeclimate.com/downloads/test-reporter/test-reporter-latest-linux-amd64 > ./cc-test-reporter
+#   chmod +x ./cc-test-reporter
+#   ./cc-test-reporter before-build
+# }
 
-function database() {
-  psql -U postgres -a -f ./database/bootstrap.sql
-}
+# function database() {
+#   psql -U postgres -a -f ./database/bootstrap.sql
+# }
 
-function envs() {
-  cp .env.example .env  
-}
+# function envs() {
+#   cp .env.example .env  
+# }
 
-coverage
-database
-envs
+# coverage
+# database
+# envs
